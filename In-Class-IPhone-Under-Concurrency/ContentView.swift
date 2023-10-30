1d07bdd8084301e43c9a62332bde9fe832b71d39
neddington <neddington@cascadia.edu>
2023-10-30 16:16:09 -0700

    Initial Commit

---
+++ In-Class-IPhone-Under-Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class-IPhone-Under-Concurrency
+//
+//  Created by Eddington, Nick on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class-IPhone-Under-Concurrency/In_Class_IPhone_Under_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_IPhone_Under_ConcurrencyApp.swift
+//  In-Class-IPhone-Under-Concurrency
+//
+//  Created by Eddington, Nick on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_IPhone_Under_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
